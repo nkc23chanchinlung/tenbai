8f3e6a01d874196aa20eafdf814f95c3df1d47ab
nkc23chanchinlung <134259379+nkc23chanchinlung@users.noreply.github.com>
2025-04-16 17:04:21 +0900

    cricle created

--- Assets/EnemyManager.cs
+++ Assets/EnemyManager.cs
@@ -7,14 +7,14 @@ public class EnemyManager : MonoBehaviour
 {
     [SerializeField] GameObject enemyPrefab;
     public int maxEnemies = 5;
-    int size = 10;
+    public int size = 10;
     // Start is called before the first frame update
     void Start()
     {
         for(int i = 0; i < maxEnemies; i++)
         {
-           float posx=Mathf.Sin(Mathf.PI * i/maxEnemies)*size;
-           float posy=Mathf.Cos(Mathf.PI * i/ maxEnemies)*size;
+           float posx=Mathf.Sin(Mathf.PI * 2*i/maxEnemies)*size;
+           float posy=Mathf.Cos(Mathf.PI * 2*i/ maxEnemies)*size;
             Instantiate(enemyPrefab, new Vector3(posx, posy, 0), quaternion.identity);
         }
     }
