0e105e4bb36f5a8bde8df2c43e0b5480a66344a2
nkc23chanchinlung <134259379+nkc23chanchinlung@users.noreply.github.com>
2025-04-24 14:57:05 +0900

    no change

--- Assets/Script/Boss/FireBoss.cs
+++ Assets/Script/Boss/FireBoss.cs
@@ -2,17 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum BossState
+{
+    Idle,
+    Move,
+    Attack,
+    Dead
+}
 public class FireBoss : MonoBehaviour
 {
+    
+    [Header("Prefebs")]
+    [SerializeField]Transform player;
+    [SerializeField] GameObject area;
+    [Header("State")]
+    [SerializeField] private int Speed;
+    [SerializeField] int Hp_Max;
+    [SerializeField] float Hp;
+    BossState state;
     // Start is called before the first frame update
     void Start()
     {
-        
+        state = BossState.Move;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        Debug.Log(state);
     }
+   private void Move()
+    { 
+    if(state == BossState.Move)
+        {
+           
+            // Move to player
+            Vector3 direction = (player.position - transform.position).normalized;
+            transform.position += direction * Time.deltaTime*Speed;
+        }
+    }
+    
 }
